d073bf8152b7ec5a3d5e1e8163fd8a4f8635b9b8
sharmakhusboo6419-hue <sharmakhusboo6419@gmail.com>
2026-09-02 00:18:28 +0530

    push the files

--- sih-idr-project/mobile_app/app/index.tsx
+++ sih-idr-project/mobile_app/app/index.tsx
@@ -15,7 +15,7 @@ export default function App() {
   useEffect(() => {
     let subAcc: any;
     let subGyro: any;
-    let timer: NodeJS.Timeout;
+    let timer: number;
 
     if (Platform.OS !== 'web') {
       Accelerometer.setUpdateInterval(100);
